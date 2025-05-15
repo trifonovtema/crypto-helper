d45f67f925c25c8c3b29fed68605ed2995740290
trifonovtema <artyom85@gmail.com>
2025-05-15 23:43:07 +0300

    change config files to samples

--- src/functions/send_from_wallet_to_wallets/config.py
+++ src/functions/send_from_wallet_to_wallets/config.py
@@ -2,8 +2,8 @@ from pydantic import BaseModel
 
 
 class Config(BaseModel):
-    source_wallet_yaml: str = "source_wallet.yaml"
-    target_wallets_yaml: str = "target_wallets.yaml"
+    source_wallet_yaml: str = "source_wallet_sample.yaml"
+    target_wallets_yaml: str = "target_wallets_sample.yaml"
     token_contract_address: str | None = None  # None if it is native token
     amount: float = 0.000001
     chain_slug: str = "bsc_testnet"  # from /src/services/chain.py
